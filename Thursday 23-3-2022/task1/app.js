ca6a492cea48bfb2c8d8d2fd91a045f9215a7da8
majdalbalawneh <majd3180157@gmail.com>
2022-03-23 16:38:45 +0200

    task

---
+++ Thursday 23-3-2022/task1/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// let x=document.createElement("p");
+// x .innerHTML="Hello world";
+// document.getElementById("one").appendChild(x);
+
+function changeText(obj,text) {
+
+   obj.innerText=text;
+
+}
