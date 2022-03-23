d8cd87b702078f23d89bbf9e3cebf03f08abc712
majdalbalawneh <majd3180157@gmail.com>
2022-03-23 07:38:47 +0200

    task

---
+++ monday21-3-2022/task2/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let sum="";
+let star="*";
+ for (let b = 0; b <=4; b++)
+  {
+     sum = sum+star;
+     console.log(sum);
+     
+ }
---
+++ monday21-3-2022/task3/app.js
@@ -0,0 +1,29 @@
+'use strice';
+
+
+let students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+let Avgmarks = 0;
+
+for (let a=0; a < students.length; a++) {
+        Avgmarks += students[a][1];
+        let avg = (Avgmarks/students.length);
+}
+
+console.log("Average grade: " + (Avgmarks)/students.length);
+
+        if ( avg < 60)
+        {
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A"); 
+}
