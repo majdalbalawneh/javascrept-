3b719a1221f5cc80cfaa15d7c13970827c593963
majdalbalawneh <majd3180157@gmail.com>
2022-03-31 12:56:49 +0200

    task

--- monday21-3-2022/task4/app.js
+++ monday21-3-2022/task4/app.js
@@ -9,6 +9,17 @@ outputs your fortune to the screen like so:
 Ex: tellFortune('software engineer', 'Jordan', 'Alice', 3);
 => "You will be a software engineer in Jordan, and married to Alice with 3 kids."
 */
+console.log('Task2');
+function tellFortune(jobTitle, location, partner, numOfChilds){
+  return ("You will be a "+jobTitle+" in "+location+", and married to "+partner+" with "+numOfChilds+" kids.");
+
+}
+
+console.log(tellFortune('software engineer', 'Jordan', 'Alice', 3));
+
+
+
+
 
 
 /*
@@ -23,6 +34,16 @@ outputs the result to the screen like so:
 Ex: calculateDogAge(1);
 => "Your doggie is 7 years old in dog years!"
 */
+console.log('Task2');
+function calculateDogAge(puppyAge){
+    let age = puppyAge*7;
+    return ("Your doggie is "+age+" years old in dog years!");
+}
+
+
+console.log(calculateDogAge(1));
+
+
 
 
 /*
@@ -36,6 +57,17 @@ outputs the result to the screen like so:
 Ex: calculateSupply(30, 3);
 => 'You will need 76650 cups of tea to last you until the ripe old age of 100;
 */
+console.log('Task3');
+
+function calculateSupply(age, ammountPerDay){
+    let teaAmmount= (100-age)*365*ammountPerDay;
+    return ("You will need "+teaAmmount+ " cups of tea to last you until the ripe old age of 100.")
+}
+
+
+console.log(calculateSupply(30, 3));
+
+
 
 
 /*
@@ -47,6 +79,13 @@ and it will return hello + name
 Ex: greet("Adam")
 => "Hello Adam"
 */
+console.log('Task4');
+function greet(name){
+    return ("Hello "+name);
+}
+
+
+console.log(greet("Adam"));
 
 
 /*
@@ -64,23 +103,36 @@ function double('7') {
   return 2 * 'x';
 }
 */
+console.log('Task5 done');
+function double(x) {
+  return 2 * x;
+}
+
+function double(num) {
+  return 2 * num;
+}
+
+function double(y) {
+  return 2 * y;
+}
 
 
+console.log('Task6 done');
 
 /*
 6
 fix these functions:
-func double1(x {
+funcion double1(x ){
   return 2 * x ;
 }
 
-functiondouble2 x)
+function double2 (x){
 return 2 * x;
 }
 
-function (x) double3 {
+function  double3(x) {
   return 2 * x;
-
+}
 */
 
 
@@ -92,6 +144,15 @@ accept 1 parameter and calculate the cube of this number
 Ex: cube(4)
 => 64
 */
+console.log('Task7 done');
+  function cube(num){
+    return Math.pow(num,3);
+}
+
+
+console.log(cube(4));
+
+
 
 
 /*
@@ -104,6 +165,16 @@ Ex: multiply(3,4)
 Ex: multiply(5,4)
 => 20
 */
+console.log('Task8');
+function multiply(num1, num2){
+    return (num1*num2);
+}
+
+
+console.log(multiply(3,4));
+console.log(multiply(5,4));
+
+
 
 
 /*
@@ -124,6 +195,21 @@ Ex: canIGetADrivingLicense(20)
 
 */
 
+console.log('Task9');
+function canIGetADrivingLicense(age){
+    if(age >= 20){
+        return "Yes you can"
+    }else{
+        return ("Please come back after "+(20-age)+" years to get one")
+    }
+}
+
+
+console.log(canIGetADrivingLicense(21));
+console.log(canIGetADrivingLicense(17));
+console.log(canIGetADrivingLicense(20));
+
+
 
 /*
 10
@@ -140,6 +226,22 @@ Ex: sameLength("tree","car")
 => false
 */
 
+console.log('Task10');
+function sameLength(string1, string2){
+    let a1= string1.length;
+    let a2= string2.length;
+    if(a1 == a2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(sameLength("tree","clue"));
+console.log(sameLength("tree","car"));
+
+
+
 /*
 11
 Write a function called largerNubmer
@@ -152,6 +254,18 @@ Ex: largerNubmer(5,6)
 Ex: largerNubmer(5,3)
 => 5
 */
+console.log('Task11');
+function largerNumber(num1, num2){
+    let largestNum= num1;
+    if (num2>num1){
+        largestNum= num2;
+    }
+    return largestNum;
+}
+
+
+console.log(largerNumber(5,6));
+console.log(largerNumber(5,3));
 
 
 /*
@@ -173,6 +287,26 @@ Ex: smallerNubmer(5,3,3)
 => 3
 
 */
+
+console.log('Task12');
+function smallerNumber(num1, num2, num3){
+    let smallest= num1;
+    if (num2<num1){
+        smallest=num2;
+    }
+    if(num3<num1 && num3<num2){
+            smallest=num3;
+    }
+    return smallest;
+}
+
+
+console.log(smallerNumber(8,6,7));
+console.log(smallerNumber(5,99,34));
+console.log(smallerNumber(5,99,3));
+console.log(smallerNumber(5,3,3));
+
+
 
 /*
 13
@@ -200,6 +334,39 @@ Ex: shorterString("air","tr","car","github","by")
 
 */
 
+	console.log('Task13');
+	console.log('Task13:---------');
+	function shorterString(s1,s2,s3,s4,s5){
+	    let s1l= s1.length;
+	    let s2l= s2.length;
+	    let s3l= s3.length;
+	    let s4l= s4.length;
+	    let s5l= s5.length;
+	    shortest=s1;
+	    if (s2l<s1l){
+	        shortest=s2;
+	    }
+	    if (s3l<s1l && s3l<s2l){
+	        shortest=s3;
+	    }
+	    if (s4l<s1l && s4l<s2l && s4l<s3l){
+	        shortest=s4;
+	    }
+	    if (s5l<s1l && s5l<s2l && s5l<s3l && s5l<s4l){
+	        shortest=s5;
+	    }
+	    return shortest;
+	}
+	
+
+	console.log(shorterString("air","school","car","by","github"));
+	console.log(shorterString("air","tr","car","by","github"));
+	console.log(shorterString("by","tr","car","air","github"));
+	console.log(shorterString("air","by","car","school","github"));
+	console.log(shorterString("air","tr","by","car","github"));
+	console.log(shorterString("air","tr","car","github","by"));
+
+
 /*
 14
 Write a function called longerString
@@ -215,6 +382,33 @@ Ex: longerString("air","schoo","car","github")
 try all the cases (change the order of the longestString)
 */
 
+console.log('Task14');
+function longerString(s1,s2,s3,s4){
+    let s1l= s1.length;
+    let s2l= s2.length;
+    let s3l= s3.length;
+    let s4l= s4.length;
+    longest=s1;
+    if (s2l>s1l){
+        longest=s2;
+    }
+    if (s3l>s1l && s3l>s2l){
+        longest=s3;
+    }
+    if (s4l>s1l && s4l>s2l && s4l>s3l){
+        longest=s4;
+    }
+    return longest;
+}
+
+
+console.log(longerString("air","school","car","github"));
+console.log(longerString("air","github","car","school"));
+
+
+
+
+
 /*
 15
 Write a function called isEven
@@ -228,6 +422,17 @@ Ex: isEven(2)
 => true
 
 */
+console.log('Task15');
+function isEven(number){
+    if(number %2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isEven(1));
+console.log(isEven(2));
 
 
 /*
@@ -243,6 +448,18 @@ Ex: isOdd(5)
 => true
 
 */
+console.log('Task16');
+function isOdd(number){
+    if(number %2 != 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+console.log(isOdd(4));
+console.log(isOdd(5));
 
 
 /*
@@ -257,6 +474,15 @@ Ex: positive(-5)
 => 5
 
 */
+console.log('Task17');
+function positive(number){
+    return Math.abs(number);
+}
+
+
+console.log(positive(4));
+console.log(positive(-5));
+
 
 
 /*
@@ -271,6 +497,15 @@ Ex: fullName("Adam","McCallen")
 Ex: fullName("Alex", "Mercer")
 => "Alex Mercer"
 */
+console.log('Task18');
+function fullName(firstName, lastNname){
+    return firstName+' '+lastNname;
+}
+
+console.log(fullName("Adam","McCallen"));
+console.log(fullName("Alex", "Mercer"));
+
+
 
 
 /*
@@ -286,7 +521,16 @@ Ex: average(5,7,9,3,5)
 => 5.8
 
 */
+console.log('Task19');
+function average(h1, h2, h3, h4, h5){
+    let sum= h1+h2+h3+h4+h5;
+    let average= sum/5;
+    return average;
+}
 
+
+console.log(average(1,2,3,4,5));
+console.log(average(5,7,9,3,5));
 
 /*
 20
@@ -303,6 +547,15 @@ Ex: randomNumber()
 
 */
 
+console.log('Task20');
+function randomNumber(){
+    return Math.random();
+}
+
+
+console.log(randomNumber());
+console.log(randomNumber());
+
 /*
 21
 Write a function called randomBetweenNumbers
@@ -317,6 +570,14 @@ Ex: randomBetweenNumbers(3,100)
 => 23
 
 */
+console.log('Task21');
+function randomBetweenNumbers(num1, num2){
+    return (num2-num1)*Math.random()+num1;
+}
+
+
+console.log(randomBetweenNumbers(1,8));
+console.log(randomBetweenNumbers(3,100));
 
 
 /*
@@ -339,6 +600,31 @@ Ex: scoreInUniversty(3)
 Ex: scoreInUniversty(71)
 => "C"
 */
+console.log('Task22');
+function scoreInUniversity(mark){
+    let grade= '';
+    if (0<=mark && mark<=49){
+        grade= 'F';
+    }
+    if (50<=mark && mark<=69){
+        grade= 'D';
+    }
+    if (70<=mark && mark<=84){
+        grade= 'C';
+    }
+    if (85<=mark && mark<=94){
+        grade= 'B';
+    }
+    if (95<=mark && mark<=100){
+        grade= 'A';
+    }
+    return grade;
+}
+
+
+console.log(scoreInUniversity(96));
+console.log(scoreInUniversity(3));
+console.log(scoreInUniversity(71));
 
 
 /*
@@ -358,6 +644,18 @@ Ex: counter()
 => 3
 
 */
+console.log('Task23');
+let countNum=0;
+function counter(){
+    return ++countNum;
+}
+
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+
 
 
 /*
